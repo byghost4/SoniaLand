dc8e8a5f79d7ff614933afe56044bdb06eb9f0ed
byghost4 <1072872260@qq.com>
2019-07-01 17:57:51 +0800

    镜头控制

---
+++ Assets/Scripts/Utils/Test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public Transform t;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Util.RotateAround(transform, t, 20 * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Utils/Util.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Util
+{
+    public static void RotateAround(Transform t,Transform target,float angle)
+    {
+        t.RotateAround(target.position, target.up, angle);
+    }
+}
